6a536e3bd77d90c146e1653fad937379aac81d5f
Songib <songibdey2@gmail.com>
2020-08-21 13:13:17 +0600

    Design Part Done

---
+++ profile.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Area</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	
+	
+
+	<div class="wrap ">
+		<a class="btn btn-sm btn-primary" href="data.php">All Users</a>
+		<div class="card shadow-sm">
+			<div class="card-body">
+				<img class="shadow-sm" style=" width:200px; height: 200px; border-radius: 50%; border:10px solid #fff; margin: auto; display: block;" src="assets/media/img/pp_photo/american-passport-photo.jpg" alt="">
+				<h2>Borsha Sarker</h2>
+				<hr>
+				<table class="table table-stiped">
+					<tr>
+						<td>Name :</td>
+						<td>Borsha Sarker</td>
+					</tr>
+					<tr>
+						<td>Name :</td>
+						<td>Borsha Sarker</td>
+					</tr>
+					<tr>
+						<td>Name :</td>
+						<td>Borsha Sarker</td>
+					</tr>
+					<tr>
+						<td>Name :</td>
+						<td>Borsha Sarker</td>
+					</tr>
+					<tr>
+						<td>Name :</td>
+						<td>Borsha Sarker</td>
+					</tr>
+				</table>
+				<div class="card-footer">
+				<a href="">Logout</a>
+			</div>
+			</div> 
+			
+		</div>
+	</div>
+	
+   <br>
+   <br>
+   <br>
+   <br>
+
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
